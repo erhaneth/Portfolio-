67d38240ee981123eb45b8fc9abbe33143069915
“erhaneth” <gumusucb21@gmail.com>
2022-07-24 15:24:12 -0700

    Added images

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import contactImg from '../public/contact.jpg';
+import ContactImg from '../public/contact.jpg'
+import Image from "next/image";
 
 export default function Contact() {
   return (
@@ -8,13 +9,13 @@ export default function Contact() {
         <p className="text-xl tracking-widest uppercase text-[#090c14]">
           Contact
         </p>
-        <h2 className="py-4">Any Question ?</h2>
+        <h2 className="py-4">Contact with me</h2>
         <div className="grid lg:grid-cols-5 gap-8">
             {/* right */}
           <div className="col-span-3 lg:col-span-2 w-full shadow-xl shadow-gray-300 rounded-xl p-4">
             <div className="lg:p-4 h-full">
                 <div>
-                    <img className="rounded-xl hover:scale-90 ease-in duration-300" src={contactImg} alt="/"/>
+                    <Image className="rounded-xl hover:scale-90 ease-in duration-300" src={ContactImg} alt="/"/>
                 </div>
             </div>
           </div>
