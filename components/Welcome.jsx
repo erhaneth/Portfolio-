b573741764b55ce7ed8a81712b47a5e9dce22e2e
“erhaneth” <gumusucb21@gmail.com>
2022-08-09 10:48:33 -0700

    Some Update

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -10,15 +10,13 @@ export default function Welcome() {
     <div id='home' className="text-center">
       <div className="md:flex-row flex-col flex justify-center items-center">
         <div className="">
-          <h1 className="py-8 text-[#090c14]">
+          <h1 className="py-20 text-[#090c14]">
             <p className="mb-5">
               Hello, this is <span className="text-[#fccb27]">Heg.</span>
             </p>
             <p>A Software Developer based on San Mateo</p>
           </h1>
-          <p className="py-16 text-gray-600 max-w-[%35] m-auto">
-            
-          </p>
+         
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
               className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
@@ -49,8 +47,8 @@ export default function Welcome() {
             </Link>
           </div>
         </div>
-        <div className=" h-auto m-auto shadow-xl shadow-gray-600 rounded-xl flex items-center justify-center p-4">
-            <Image src={AboutImg} className="rounded-3xl" alt="/" 
+        <div className="h-auto m-auto shadow-xl shadow-gray-600 rounded-full flex items-center justify-center p-4">
+            <Image src={AboutImg} className=" rounded-full" alt="/" 
              width="450px"
              height="450px"
             />
