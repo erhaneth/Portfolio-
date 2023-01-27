ecf74992607e46a83341adb76a730c69301f6d36
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-27 13:39:37 -0800

    changed profile image

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Link from "next/link";
-import { AiOutlineMail } from 'react-icons/ai';
-import { FaGithub, FaLinkedinIn,FaRegFilePdf } from "react-icons/fa";
-import WelcomeImg from "../public/welcome.jpg";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn, FaRegFilePdf } from "react-icons/fa";
+import WelcomeImg from "../public/hegprofile.png";
 import Image from "next/image";
 
 export default function Welcome() {
   return (
-    <div id='home' className="text-center">
+    <div id="home" className="text-center">
       <div className="md:flex-row flex-col flex justify-center items-center">
         <div className="">
           <h1 className="py-20 text-[#090c14]">
             <p className="mb-5">
-              Hello, this is <span className="text-[#fccb27]">Heg.</span>
+              Hello, this is <span className="text-[#fccb27]">Huseyin.</span>
             </p>
-            <p>A Software Developer based in the San Francisco Bay Area, CA</p>
+            <p>A Software Engineer based in the San Francisco Bay Area, CA</p>
           </h1>
-         
+
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <a
               className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
@@ -31,9 +31,7 @@ export default function Welcome() {
               target="_blank"
               rel="noreferrer"
             >
-              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
-             
-              >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
                 <FaGithub />
               </div>
             </a>
@@ -42,28 +40,30 @@ export default function Welcome() {
               // target="_blank"
               // rel="noreferrer"
             >
-              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300"
-             
-              >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
                 <FaRegFilePdf />
               </div>
             </a>
-            <Link href='mailto:gumusucb21@gmail.com'
-            target="_blank"
+            <Link
+              href="mailto:gumusucb21@gmail.com"
+              target="_blank"
               rel="noreferrer"
             >
-              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <AiOutlineMail />
               </div>
             </Link>
           </div>
         </div>
         <div className="h-auto m-auto shadow-xl shadow-gray-600 rounded-full flex items-center justify-center p-4">
-            <Image src={WelcomeImg } className=" rounded-full" alt="/" 
-             width="450px"
-             height="450px"
-            />
-          </div>
+          <Image
+            src={WelcomeImg}
+            className=" rounded-full"
+            alt="/"
+            width="450px"
+            height="450px"
+          />
+        </div>
       </div>
     </div>
   );
