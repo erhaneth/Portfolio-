78d5264fbd3cf9c9a8703d70f009ac156ed1c06c
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-01-29 09:27:27 -0800

    changed styling

--- components/about.jsx
+++ components/about.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Image from "next/image";
-import AboutImg from "../public/about.jpg";
 
 export default function About() {
   return (
-    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+    <div
+      id="about"
+      className="w-full md:h-screen p-2 flex items-center py-16 bg-[#f5f5f5]"
+    >
       <div className="w-full md:screen p-2 flex items-center py-10">
-        <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-          <div className="col-span-2 max-w-lg mx-auto">
+        <div className="max-w-1240px m-auto md:grid grid-cols-1 gap-8">
+          <div className="col-span-1 max-w-lg mx-auto">
             <p1 className="uppercase flex justify-center text-xl tracking-widest text-[#090c14]">
               About
             </p1>
@@ -32,9 +33,6 @@ export default function About() {
               </span>{" "}
               work environment.
             </p>
-          </div>
-          <div className="w-full  h-auto m-auto shadow-xl shadow-gray-600 rounded-xl flex items-center justify-center p-4 ">
-            <Image src={AboutImg} className="rounded-3xl" alt="/" />
           </div>
         </div>
       </div>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,42 +1,43 @@
-//built in link from next.js
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import NavLogo from "../public/navLogo.png";
-import {  AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export default function Navbar() {
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
   };
-  
 
   return (
-    <div className="bg-[#797b81] text-white font-bold fixed w-full h-20 shadow-2xl z-50">
+    <div className="bg-gray-800 text-white font-bold fixed w-full h-20 shadow-2xl z-50">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image src={NavLogo} alt="/" width="160" height="160" />
         {nav && (
-        <div className="bg-[#797b81] absolute top-20 right-0">
-        <ul className="">
-            <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
-            </Link>
-            <Link href="/#about">
-              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
-            </Link>
-            <Link href="/#skills">
-              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
-            </Link>
-            <Link href="/#projects">
-              <li className="ml-10 text-sm uppercase hover:border-b">
-                Projects
-              </li>
-            </Link>
-          </ul>
-
-        </div>
-         ) }
+          <div className="bg-gray-800 absolute top-20 right-0">
+            <ul className="">
+              <Link href="/">
+                <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+              </Link>
+              <Link href="/#about">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  About
+                </li>
+              </Link>
+              <Link href="/#skills">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  Skills
+                </li>
+              </Link>
+              <Link href="/#projects">
+                <li className="ml-10 text-sm uppercase hover:border-b">
+                  Projects
+                </li>
+              </Link>
+            </ul>
+          </div>
+        )}
         <div className="text-white text-2xl">
           <ul className="hidden md:flex">
             <Link href="/">
@@ -55,8 +56,8 @@ export default function Navbar() {
             </Link>
           </ul>
           {/* Hamburger Icon */}
-          <div onClick={handleNav} className="md:hidden">
-            <AiOutlineMenu size={25} />
+          <div onClick={handleNav} className="md:hidden cursor-pointer">
+            <AiOutlineMenu size={25} className="animate-menu-icon" />
           </div>
         </div>
       </div>
